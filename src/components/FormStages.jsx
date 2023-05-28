d78ef1acb7438312ec3b709493854c733ec21a40
itsYashASeeker <yashkc6432@gmail.com>
2023-05-28 21:53:27 +0530

    stage added

--- src/components/FormStages.jsx
+++ src/components/FormStages.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react';
+import "../css/formStages.css"
 
 const FormStages = () => {
+
+  const [prevS, setPS] = useState("1S");
+
+  function selStage(eT){
+    if(eT.id==prevS){
+      return false;
+    }
+    document.getElementById(eT.id).classList.add("selected");
+    document.getElementById(prevS).classList.remove("selected");
+    setPS(eT.id);
+  }
+
   return (
-    <div>
-      <div className='flex'>
-        Form Stages
-      </div>
+    <div className='stages'>
+      <button id='1S' onClick={(e)=>{selStage(e.target)}} className='selected'>1</button>
+      <span></span>
+      <button id='2S' onClick={(e)=>{selStage(e.target)}}>2</button>
+      <span></span>
+      <button id='3S' onClick={(e)=>{selStage(e.target)}}>3</button>
+      <span></span>
+      <button id='4S' onClick={(e)=>{selStage(e.target)}}>4</button>
+      <span></span>
+      <button id='5S' onClick={(e)=>{selStage(e.target)}}>5</button>
+      <span></span>
+      <button id='6S' onClick={(e)=>{selStage(e.target)}}>6</button>
     </div>
   )
 }
--- src/components/Forms.jsx
+++ src/components/Forms.jsx
@@ -2,11 +2,28 @@ import React, { useState } from 'react';
 import FormStages from './FormStages';
 import OutsatndingInstForm from '../data/OutsatndingInstForm';
 import Field from './utils/Field';
+import "../css/formStages.css";
 
 const Forms = () => {
   const limit = 8;
   const [current, setCurrent] = useState(0);
   const [formData, setFormData] = useState({});
+
+  const [prevS, setPS] = useState("1S");
+
+  function selStage(eT) {
+    if (eT.id == prevS) {
+      return false;
+    }
+    document.getElementById(eT.id).classList.add("selected");
+    document.getElementById(prevS).classList.remove("selected");
+    setPS(eT.id);
+    var g = Number(eT.id.split("")[0])-1;
+    if (g <= limit) {
+      setCurrent(g);
+    }
+    window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
+  }
 
   const handleNext = () => {
     if (current < limit) {
@@ -51,7 +68,25 @@ const Forms = () => {
 
   return (
     <div className="">
-      <FormStages />
+      <div className='stages'>
+        <button id='1S' onClick={(e) => { selStage(e.target) }} className='selected'>1</button>
+        <span></span>
+        <button id='2S' onClick={(e) => { selStage(e.target) }}>2</button>
+        <span></span>
+        <button id='3S' onClick={(e) => { selStage(e.target) }}>3</button>
+        <span></span>
+        <button id='4S' onClick={(e) => { selStage(e.target) }}>4</button>
+        <span></span>
+        <button id='5S' onClick={(e) => { selStage(e.target) }}>5</button>
+        <span></span>
+        <button id='6S' onClick={(e) => { selStage(e.target) }}>6</button>
+        <span></span>
+        <button id='7S' onClick={(e) => { selStage(e.target) }}>7</button>
+        <span></span>
+        <button id='8S' onClick={(e) => { selStage(e.target) }}>8</button>
+        <span></span>
+        <button id='9S' onClick={(e) => { selStage(e.target) }}>9</button>
+      </div>
 
       <div className="border-black border-[1] mx-auto rounded-xl shadow-2xl bg-0xFAF9F6 p-3 w-[70%]">
         <div className="text-sm bg-red-300 font-bold italic rounded-lg p-3 my-5">
